2521c089be06f153469e8c0496accaee76c1b28c
izumin5210 <m@izum.in>
2017-12-11 13:56:09 +0900

    Add Pool interface

--- pool.go
+++ pool.go
@@ -1,1 +1,10 @@
 package nrredigo
+
+import (
+	"github.com/garyburd/redigo/redis"
+)
+
+// Pool is an interface for representing a pool of Redis connections
+type Pool interface {
+	Get() redis.Conn
+}
