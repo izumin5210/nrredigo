58d39dcb5dabad5ba139ec3982c06e00d20bb110
izumin5210 <m@izum.in>
2018-12-24 01:23:31 +0900

    Make command name lower

--- conn.go
+++ conn.go
@@ -1,6 +1,8 @@
 package nrredigo
 
 import (
+	"strings"
+
 	"github.com/gomodule/redigo/redis"
 	"github.com/newrelic/go-agent"
 )
@@ -21,8 +23,7 @@ type wrappedConn struct {
 
 func (c *wrappedConn) Do(commandName string, args ...interface{}) (interface{}, error) {
 	if c.txn != nil {
-		seg := c.createSegment()
-		seg.Operation = commandName
+		seg := c.createSegment(commandName)
 		seg.ParameterizedQuery = formatCommand(commandName, args)
 		defer seg.End()
 	}
@@ -31,8 +32,7 @@ func (c *wrappedConn) Do(commandName string, args ...interface{}) (interface{}, 
 
 func (c *wrappedConn) Send(commandName string, args ...interface{}) error {
 	if c.txn != nil {
-		seg := c.createSegment()
-		seg.Operation = commandName
+		seg := c.createSegment(commandName)
 		seg.ParameterizedQuery = formatCommand(commandName, args)
 		defer seg.End()
 	}
@@ -41,8 +41,7 @@ func (c *wrappedConn) Send(commandName string, args ...interface{}) error {
 
 func (c *wrappedConn) Flush() error {
 	if c.txn != nil {
-		seg := c.createSegment()
-		seg.Operation = "flush"
+		seg := c.createSegment("flush")
 		defer seg.End()
 	}
 	return c.Conn.Flush()
@@ -50,17 +49,17 @@ func (c *wrappedConn) Flush() error {
 
 func (c *wrappedConn) Receive() (interface{}, error) {
 	if c.txn != nil {
-		seg := c.createSegment()
-		seg.Operation = "receive"
+		seg := c.createSegment("receive")
 		defer seg.End()
 	}
 	return c.Conn.Receive()
 }
 
-func (c *wrappedConn) createSegment() newrelic.DatastoreSegment {
+func (c *wrappedConn) createSegment(cmdName string) newrelic.DatastoreSegment {
 	return newrelic.DatastoreSegment{
 		StartTime:    newrelic.StartSegmentNow(c.txn),
 		Product:      newrelic.DatastoreRedis,
+		Operaiton:    strings.ToLower(cmdName),
 		Host:         c.cfg.Host,
 		PortPathOrID: c.cfg.PortPathOrID,
 		DatabaseName: c.cfg.DBName,
